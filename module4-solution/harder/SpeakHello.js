c916b8f9547916a54337c7c4f1fe87989b119990
adsauas <adsauas@gmail.com>
2024-11-22 09:24:54 +0500

    module-4Sol2

--- module4-solution/harder/SpeakHello.js
+++ module4-solution/harder/SpeakHello.js
@@ -1,10 +1,12 @@
-
 (function (window) {
   var speakWord = "Hello";
 
   var helloSpeaker = {
     speak: function (name) {
       console.log(speakWord + " " + name);
+    },
+    speakHTML: function (name) {
+      return `<p>${speakWord} ${name}</p>`;
     }
   };
 
